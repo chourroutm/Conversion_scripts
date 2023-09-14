fc29066e7a058ff94e5fc00860ad427274e7bba6
Matthieu Chourrout <83714683+chourroutm@users.noreply.github.com>
2023-09-14 23:50:58 +0200

    Add files via upload

---
+++ convertMultiRawVols.py
@@ -0,0 +1,61 @@
+# Import os module to read directory
+import os
+import argparse
+#import sys
+import numpy as np
+from shutil import copyfile
+
+def main(keyword):
+	intensity_range_origin = np.array([0, 1],dtype=float)
+	intensity_range_export = np.array([0, 65535],dtype=float)
+	# VALUES TO SET:
+	#     dirpath_origin -- absolute path to the folder with all the acquisitions
+	#     dirpath_export -- absolute path to the folder with the resampled recons
+	#     intensity_range_origin -- bounds of the 32-bit floating point intensity in raw images
+	#     intensity_range_export -- bounds of the 16-bit unsigned integer intensity in raw images
+	#     dirs_to_skip -- acquisition folders to skip from export
+	dirpath_origin = '.'
+	dirpath_export = 'raw_uint16_to_go' # -- see below --
+	dirpath_export = os.path.join(dirpath_origin,dirpath_export) # the folder to export to will be relative to the main folder
+	intensity_range_origin[0] = 0.1
+	intensity_range_origin[1] = 0.25
+	#intensity_range_export[0] = <new_value> # keep commented for default values
+	#intensity_range_export[1] = <new_value> # keep commented for default values
+	dirs_to_skip = [ 'raw_uint16_to_go',
+	'rsync_raw_data_to_ruche.sh',
+	'031_EndoStrokeR21_450mA_u18gap8p5mm_Cu100um_Au20um_mt1_1x_lightning_35ms_3600proj_360_'
+	]
+
+	main_dir_list = os.listdir(dirpath_origin)
+	for acquisition_dir in main_dir_list:
+		if acquisition_dir in dirs_to_skip or keyword.lower() not in acquisition_dir.lower():
+			print("skipped folder:", acquisition_dir)
+			continue
+		acquisition_dir_list = os.listdir(os.path.join(dirpath_origin,acquisition_dir))
+		if not os.path.exists(os.path.join(dirpath_export,acquisition_dir)):
+			os.mkdir(os.path.join(dirpath_export,acquisition_dir))
+		for entry in acquisition_dir_list:
+			if entry.endswith(".par"): # recon parameter file
+				print("par file found:", entry)
+				copyfile(os.path.join(dirpath_origin,acquisition_dir,entry),os.path.join(dirpath_export,acquisition_dir,entry))
+			if entry.endswith(".vol"): # recon volume
+				print("vol file found:", entry)
+				data = np.fromfile(os.path.join(dirpath_origin,acquisition_dir,entry),dtype=np.dtype('<f4'))
+				data = (data-intensity_range_origin[0]) / (intensity_range_origin[1]-intensity_range_origin[0]) * ((intensity_range_export[1]-intensity_range_export[0])) + intensity_range_export[0]
+				data[data<intensity_range_export[0]] = intensity_range_export[0]
+				data[data>intensity_range_export[1]] = intensity_range_export[1]
+				data.astype(np.dtype('<u2')).tofile(os.path.join(dirpath_export,acquisition_dir,entry))
+				logf = open(os.path.join(dirpath_export,acquisition_dir,"values.txt"), "w")
+				logf.write("min = %.5f\nmax = %.5f" % (intensity_range_origin[0], intensity_range_origin[1]))
+				logf.close()
+
+parser = argparse.ArgumentParser(
+                    prog='convertMultiRawVols',
+                    description='convertMultiRawVols',
+                    epilog='convertMultiRawVols')
+					
+parser.add_argument('keyword')
+
+args = parser.parse_args()
+
+main(args.keyword)
